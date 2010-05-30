47abf2409ad461a3ee14879563c3d3776a89ae06
Wally Glutton <stungeye@gmail.com>
2010-05-30 09:21:53 -0500

    Optimized circular array. Improved test timer method.

--- lib/glutton_ratelimit.rb
+++ lib/glutton_ratelimit.rb
@@ -1,31 +1,23 @@
 class GluttonRatelimit
-  
-  class UnknownTimestampSelected < StandardError; end
+  attr_reader :executions
   
   def initialize executions, time_period
     @executions = executions
     @time_period = time_period
-    
     @timestamps = Array.new executions, (Time.now - time_period)
-    @timestamps_position = 0
   end
   
   def oldest_timestamp
-    fetch_position = @timestamps_position + 1
-    fetch_position = 0 if fetch_position == @timestamps.size
-    @timestamps[fetch_position]
+    @timestamps[0]
   end
   
   def current_timestamp= new_stamp
-    @timestamps_position += 1
-    @timestamps_position = 0 if @timestamps_position == @timestamps.size
-    @timestamps[@timestamps_position] = new_stamp
+    @timestamps.push(new_stamp).shift
   end
     
   def wait
     delta = Time.now - oldest_timestamp
     sleep(@time_period - delta) if delta < @time_period
-    current_time = Time.now
     self.current_timestamp = Time.now
   end
 end
--- test/test_glutton_ratelimit.rb
+++ test/test_glutton_ratelimit.rb
@@ -8,26 +8,44 @@ class TestGluttonRatelimit < Test::Unit::TestCase
   
   def test_120_tasks_every_second_with_ms_task
     min_time = 1
-    delta = timed_run(120, min_time) {sleep 0.001}
+    rl = GluttonRatelimit.new 120, min_time
+    delta = timed_run(rl) {sleep 0.001}
+    assert delta > min_time
+  end
+
+  def test_120_tasks_every_quarter_second_with_ms_task
+    min_time = 0.25
+    rl = GluttonRatelimit.new 120, min_time
+    delta = timed_run(rl) {sleep 0.001}
     assert delta > min_time
   end
   
   def test_120_tasks_every_second_with_cenisecond_task
     min_time = 1
-    delta = timed_run(120, min_time) {sleep 0.01}
+    rl = GluttonRatelimit.new 120, min_time
+    delta = timed_run(rl) {sleep 0.01}
+    assert delta > min_time
+  end
+  
+  def test_50_tasks_every_second_with_fast_task
+    min_time = 1
+    rl = GluttonRatelimit.new 50, min_time
+    delta = timed_run(rl) {sleep 0}
     assert delta > min_time
   end
   
   def test_1_task_every_2_seconds_with_ms_task
     min_time = 2
-    delta = timed_run(1, min_time) {sleep 0.001}
+    rl = GluttonRatelimit.new 1, min_time
+    delta = timed_run(rl) {sleep 0.001}
     assert delta > min_time
   end
   
   def test_1_task_every_2_seconds_with_2_second_task
     min_time = 2
-    delta = timed_run(1, min_time) {sleep 2}
-    #puts "#{delta} expected > #{min_time}"
+    rl = GluttonRatelimit.new 1, min_time
+    delta = timed_run(rl) {sleep 2}
+    #puts "#{delta} >? #{min_time}"
     assert delta > min_time
   end
 end
